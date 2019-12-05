45e1a83c3d8f943dc16350cea6867e5a143dfc7f
saubyk <39208279+saubyk@users.noreply.github.com>
2019-12-04 20:27:43 -0500

    Payments swagger added

--- controllers/payments.js
+++ controllers/payments.js
@@ -3,6 +3,87 @@
 //Function # 1
 //Invoke the 'pay' command to pay the bolt11 invoice passed with the argument
 //Arguments - Bolt11 Invoice [required]
+/**
+* @swagger
+* /pay:
+*   post:
+*     tags:
+*       - Payments
+*     name: pay
+*     summary: Pay a BOLT11 invoice
+*     consumes:
+*       - application/json
+*     parameters:
+*       - in: body
+*         name: invoice
+*         description: BOLT11 invoice
+*         type: string
+*         required:
+*           - invoice
+*       - in: body
+*         name: amount
+*         description: Amount in milli satoshis
+*         type: integer
+*     responses:
+*       201:
+*         description: OK
+*         schema:
+*           type: object
+*           properties:
+*             id:
+*               type: integer
+*               description: id
+*             payment_hash:
+*               type: string
+*               description: payment_hash
+*             destination:
+*               type: string
+*               description: destination
+*             msatoshi:
+*               type: integer
+*               description: msatoshi
+*             amount_msat:
+*               type: string
+*               description: amount_msat
+*             msatoshi_sent:
+*               type: integer
+*               description: msatoshi_sent
+*             amount_sent_msat:
+*               type: string
+*               description: amount_sent_msat
+*             created_at:
+*               type: integer
+*               description: created_at
+*             status:
+*               type: string
+*               description: status
+*             payment_preimage:
+*               type: string
+*               description: payment_preimage
+*             bolt11:
+*               type: string
+*               description: bolt11
+*       500:
+*         description: Server error
+*         schema:
+*           type: object
+*           properties:
+*             type:
+*               type: string
+*               description: type
+*             name:
+*               type: string
+*               description: name
+*             message:
+*               type: string
+*               description: message
+*             code:
+*               type: integer
+*               description: code
+*             fullType:
+*               type: string
+*               description: fullType
+*/
 exports.payInvoice = (req,res) => {
     global.logger.log('pay initiated...');
 
@@ -41,6 +122,44 @@ exports.payInvoice = (req,res) => {
 //Function # 2
 //Invoke the 'listpays' command to list all the payments attempted from the node
 //Arguments - Bolt11 invoice [optional]
+/**
+* @swagger
+* /pay/listPays:
+*   get:
+*     tags:
+*       - Payments
+*     name: listpays
+*     summary: Returns a list of payments
+*     parameters:
+*       - in: query
+*         name: invoice
+*         description: BOLT11 invoice
+*         type: invoice
+*     responses:
+*       200:
+*         description: An array of pays objects is returned
+*         schema:
+*           type: object
+*           properties:
+*             pays:
+*               type: object
+*               properties:
+*                 bolt11:
+*                   type: string
+*                   description: bolt11
+*                 status:
+*                   type: string
+*                   description: status
+*                 payment_preimage:
+*                   type: string
+*                   description: payment_preimage
+*                 amount_sent_msat:
+*                   type: string
+*                   description: amount_sent_msat
+*               description: pays
+*       500:
+*         description: Server error
+*/
 exports.listPays = (req,res) => {
     global.logger.log('listPays initiated...');
 
@@ -76,6 +195,65 @@ exports.listPays = (req,res) => {
 //Function # 3
 //Invoke the 'listpayments' command to list all the payments attempted from the node
 //Arguments - Bolt11 invoice [optional]
+/**
+* @swagger
+* /pay/listPayments:
+*   get:
+*     tags:
+*       - Payments
+*     name: listpayments
+*     summary: Returns a detailed list of payments
+*     parameters:
+*       - in: query
+*         name: invoice
+*         description: BOLT11 invoice
+*         type: invoice
+*     responses:
+*       200:
+*         description: An array of payments objects is returned
+*         schema:
+*           type: object
+*           properties:
+*             payments:
+*               type: object
+*               properties:
+*                 id:
+*                   type: integer
+*                   description: id
+*                 payment_hash:
+*                   type: string
+*                   description: payment_hash
+*                 destination:
+*                   type: string
+*                   description: destination
+*                 msatoshi:
+*                   type: integer
+*                   description: msatoshi
+*                 amount_msat:
+*                   type: string
+*                   description: amount_msat
+*                 msatoshi_sent:
+*                   type: integer
+*                   description: msatoshi_sent
+*                 amount_sent_msat:
+*                   type: string
+*                   description: amount_sent_msat
+*                 created_at:
+*                   type: integer
+*                   description: created_at
+*                 status:
+*                   type: string
+*                   description: status
+*                 payment_preimage:
+*                   type: string
+*                   description: payment_preimage
+*                 bolt11:
+*                   type: string
+*                   description: bolt11
+*               description: payments
+*       500:
+*         description: Server error
+*/
 exports.listPayments = (req,res) => {
     global.logger.log('listPayments initiated...');
 
