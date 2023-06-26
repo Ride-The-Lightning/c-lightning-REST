8220f61b21399f09be3b4f21e6aa02f9e51ef2e1
Shahana Farooqui <shahana.farooqui@gmail.com>
2023-06-26 10:27:43 -0700

    Version compatibility check fix #187
    Version compatibility check fix #187

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,21 @@
 const isVersionCompatible = (currentVersion, checkVersion) => {
     if (currentVersion) {
-      const currentVersionArr = currentVersion.trim()?.replace('v', '').split('-')[0].split('.') || [];
-      currentVersionArr[1] = currentVersionArr[1].substring(0, 2);
-      const checkVersionsArr = checkVersion.split('.');
-      checkVersionsArr[1] = checkVersionsArr[1].substring(0, 2);
-      return (+currentVersionArr[0] > +checkVersionsArr[0]) ||
-        (+currentVersionArr[0] === +checkVersionsArr[0] && +currentVersionArr[1] > +checkVersionsArr[1]) ||
-        (+currentVersionArr[0] === +checkVersionsArr[0] && +currentVersionArr[1] === +checkVersionsArr[1] && +currentVersionArr[2] >= +checkVersionsArr[2]);
+      let pattern = /v(\d+(\.\d+)*)/; 
+      let match = currentVersion.match(pattern);
+      if (match && match.length && match.length > 1) {
+        logger.log('Global Version: ' + match[1]);
+        logger.log('Checking Compatiblility with Version: ' + checkVersion);
+        const currentVersionArr = match[1].split('.') || [];
+        currentVersionArr[1] = currentVersionArr[1].substring(0, 2);
+        const checkVersionsArr = checkVersion.split('.');
+        checkVersionsArr[1] = checkVersionsArr[1].substring(0, 2);
+        return (+currentVersionArr[0] > +checkVersionsArr[0]) ||
+          (+currentVersionArr[0] === +checkVersionsArr[0] && +currentVersionArr[1] > +checkVersionsArr[1]) ||
+          (+currentVersionArr[0] === +checkVersionsArr[0] && +currentVersionArr[1] === +checkVersionsArr[1] && +currentVersionArr[2] >= +checkVersionsArr[2]);
+      } else {
+        logger.warn('Invalid Version String');
+        return false;
+      }
     }
     return false;
 };
